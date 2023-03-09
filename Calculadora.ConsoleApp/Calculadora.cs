f94922361b18beb78147e6a69554d84966200762
DAVI <DAVI@DESKTOP-DFB>
2023-03-09 15:15:04 -0300

    Add project files.

---
+++ Calculadora.ConsoleApp/Calculadora.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.ConsoleApp
+{
+    internal class Calculadora
+    {
+        public double NumA { get; set; }
+        public double NumB { get; set; }
+        public char Operacao { get; set; }
+        public double Resultado { get; set; }
+        public Calculadora(double num1, double num2)
+        {
+            NumA = num1;
+            NumB = num2;
+        }
+        public Calculadora(char operacao)
+        {
+            Operacao = operacao;
+        }
+        public void Adicao()
+        {
+            Resultado = NumA + NumB;
+            Operacao = '+';
+        }
+        public void Subtracao()
+        {
+            Resultado = NumA - NumB;
+            Operacao = '-';
+        }
+        public void Divisao()
+        {
+            Resultado = NumA / NumB;
+            Operacao = '/';
+        }
+        public void Multiplicacao()
+        {
+            Resultado = NumA * NumB;
+            Operacao = '*';
+        }
+        public void Modulo()
+        {
+            Resultado = NumA % NumB;
+            Operacao = '%';
+        }
+        public void GerarTabuada()
+        {
+            for(
+                int i = 1; i <= NumB; i++) 
+            {
+                Resultado = NumA * i;
+                Console.WriteLine(NumA + " x " + i + " = " + Resultado);
+            }
+        }
+        public void Erro()
+        {
+            Console.WriteLine("Tente novamente e selecione uma operacao disponivel");
+        }
+    }
+}
---
+++ Calculadora.ConsoleApp/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Calculadora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = null;
+
+            while (true) 
+            {
+                double entradaNumA;
+                double entradaNumB;
+                char entradaOperacao;
+                Console.Clear();
+                Console.WriteLine("============================[ CTRL + C ]=");
+                Console.WriteLine("========  Calculadora Top 2023  =========");
+                Console.WriteLine("=========================================");
+                Console.WriteLine("Selecione uma das operacoes disponiveis: ");
+                Console.WriteLine(" + = Adicao");
+                Console.WriteLine(" - = Subtracao");
+                Console.WriteLine(" * = Multiplicacao");
+                Console.WriteLine(" / = Divisao");
+                Console.WriteLine(" % = Modulo da divisao");
+                Console.WriteLine(" # = Gera tabuada do numero desejado");
+                Console.WriteLine("=========================================");
+                Console.Write("Informe a operacao: ");
+                entradaOperacao = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("=========================================");
+                Console.Write("Informe o primeiro numero: ");
+                entradaNumA = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("=========================================");
+                Console.Write("Informe o segundo numero: ");
+                entradaNumB = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("=========================================");
+                Console.WriteLine("=========================================");
+                Console.WriteLine("=========================================");
+                if(entradaOperacao != '#' && entradaOperacao != '+' && entradaOperacao != '-' && entradaOperacao != '*' && entradaOperacao != '/' && entradaOperacao != '%')
+                {
+                    Console.WriteLine("A operacao nao existe na calculadora.");
+                }
+                switch (entradaOperacao) 
+                {
+                    case '+':
+                        calculadora = new Calculadora(entradaNumA, entradaNumB);
+                        calculadora.Adicao();
+                        break;
+                    case '-':
+                        calculadora = new Calculadora(entradaNumA, entradaNumB);
+                        calculadora.Subtracao();
+                        break;
+                    case '*':
+                        calculadora = new Calculadora(entradaNumA, entradaNumB);
+                        calculadora.Multiplicacao();
+                        break;
+                    case '/':
+                        while(entradaNumB == 0)
+                        {
+                            Console.WriteLine("O divisor nao pode ser igual a zero");
+                            Console.WriteLine("=========================================");
+                            Console.Write("Digite o divisor novamente: ");
+                            entradaNumB = Convert.ToDouble(Console.ReadLine());
+                        }
+                        calculadora = new Calculadora(entradaNumA, entradaNumB);
+                        calculadora.Divisao();
+                        break;
+                    case '%':
+                        calculadora = new Calculadora(entradaNumA, entradaNumB);
+                        calculadora.Modulo();
+                        break;
+                    case '#':
+                        calculadora = new Calculadora(entradaNumA, entradaNumB);
+                        calculadora.GerarTabuada();
+                        break;
+                    default:
+                        calculadora = new Calculadora(entradaOperacao);
+                        calculadora.Erro();
+                        continue;
+                }
+                Console.WriteLine("=============   RESULTADO   =============");
+                if(entradaOperacao != '#')
+                {
+                    Console.WriteLine($" Resultado: {calculadora.NumA} {calculadora.Operacao} {calculadora.NumB} = {calculadora.Resultado}");
+                }
+                Console.WriteLine("=========================================");
+                Console.WriteLine("Aperte enter para uma nova operacao,");
+                Console.WriteLine("ou Ctrl + C para encerrar o console.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
