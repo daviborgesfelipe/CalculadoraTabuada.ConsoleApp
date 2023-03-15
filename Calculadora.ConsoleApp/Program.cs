7f06c30970b99b84625b648f38107795984028cc
Davi Borges <davifelipe_300@hotmail.com>
2023-03-15 09:14:27 -0300

    fix: metodo arruma relatorio.

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -28,9 +28,10 @@
                 Console.WriteLine("=========================================");
                 Console.Write("Informe a operacao: ");
                 entradaOperacao = Convert.ToChar(Console.ReadLine());
-                //if (entradaOperacao == '#' || entradaOperacao == '+' || entradaOperacao == '-' || entradaOperacao == '*' || entradaOperacao == '/' || entradaOperacao == '%')
-                //{
-                //}
+                if(entradaOperacao != '#' && entradaOperacao != '+' && entradaOperacao != '-' && entradaOperacao != '*' && entradaOperacao != '/' && entradaOperacao != '%' && entradaOperacao != '@')
+                {
+                    Console.WriteLine("A operacao nao existe na calculadora.");
+                }
                 if (entradaOperacao == '@')
                 {
                     switch (entradaOperacao)
@@ -41,71 +42,67 @@
                             break;
                     }
                 }
-                Console.WriteLine("=========================================");
-                Console.Write("Informe o primeiro numero: ");
-                entradaNumA = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("=========================================");
-                Console.Write("Informe o segundo numero: ");
-                entradaNumB = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("=========================================");
-                Console.WriteLine("=========================================");
-                if(entradaOperacao != '#' && entradaOperacao != '+' && entradaOperacao != '-' && entradaOperacao != '*' && entradaOperacao != '/' && entradaOperacao != '%' && entradaOperacao != '@')
+                else
                 {
-                    Console.WriteLine("A operacao nao existe na calculadora.");
+                    Console.WriteLine("=========================================");
+                    Console.Write("Informe o primeiro numero: ");
+                    entradaNumA = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("=========================================");
+                    Console.Write("Informe o segundo numero: ");
+                    entradaNumB = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("=========================================");
+                    Console.WriteLine("=========================================");
+
+                    switch (entradaOperacao) 
+                    {
+                        case '+':
+                            calculadora = new Calculadora(entradaNumA, entradaNumB);
+                            calculadora.Adicao();
+                            break;
+                        case '-':
+                            calculadora = new Calculadora(entradaNumA, entradaNumB);
+                            calculadora.Subtracao();
+                            break;
+                        case '*':
+                            calculadora = new Calculadora(entradaNumA, entradaNumB);
+                            calculadora.Multiplicacao();
+                            break;
+                        case '/':
+                            while(entradaNumB == 0)
+                            {
+                                Console.WriteLine("O divisor nao pode ser igual a zero");
+                                Console.WriteLine("=========================================");
+                                Console.Write("Digite o divisor novamente: ");
+                                entradaNumB = Convert.ToDouble(Console.ReadLine());
+                            }
+                            calculadora = new Calculadora(entradaNumA, entradaNumB);
+                            calculadora.Divisao();
+                            break;
+                        case '%':
+                            calculadora = new Calculadora(entradaNumA, entradaNumB);
+                            calculadora.Modulo();
+                            break;
+                        case '#':
+                            calculadora = new Calculadora(entradaNumA, entradaNumB);
+                            calculadora.GerarTabuada();
+                            break;
+                        default:
+                            calculadora = new Calculadora(entradaOperacao);
+                            calculadora.Erro();
+                            continue;
+                    }
+                    if (entradaOperacao == '#' || entradaOperacao == '+' || entradaOperacao == '-' || entradaOperacao == '*' || entradaOperacao == '/' || entradaOperacao == '%')
+                    {
+                        descricao[qntdOperacao] = calculadora.NumA +" "+ calculadora.Operacao+" "+calculadora.NumB + " = " +calculadora.Resultado;
+                        qntdOperacao++;
+                    }
+                    Console.WriteLine("=============   RESULTADO   =============");
+                    if(entradaOperacao != '#')
+                    {
+                        Console.WriteLine($" Resultado: {calculadora.NumA} {calculadora.Operacao} {calculadora.NumB} = {calculadora.Resultado}");
+                    }
+                    Console.WriteLine("=========================================");
                 }
-                switch (entradaOperacao) 
-                {
-                    case '+':
-                        calculadora = new Calculadora(entradaNumA, entradaNumB);
-                        calculadora.Adicao();
-                        break;
-                    case '-':
-                        calculadora = new Calculadora(entradaNumA, entradaNumB);
-                        calculadora.Subtracao();
-                        break;
-                    case '*':
-                        calculadora = new Calculadora(entradaNumA, entradaNumB);
-                        calculadora.Multiplicacao();
-                        break;
-                    case '/':
-                        while(entradaNumB == 0)
-                        {
-                            Console.WriteLine("O divisor nao pode ser igual a zero");
-                            Console.WriteLine("=========================================");
-                            Console.Write("Digite o divisor novamente: ");
-                            entradaNumB = Convert.ToDouble(Console.ReadLine());
-                        }
-                        calculadora = new Calculadora(entradaNumA, entradaNumB);
-                        calculadora.Divisao();
-                        break;
-                    case '%':
-                        calculadora = new Calculadora(entradaNumA, entradaNumB);
-                        calculadora.Modulo();
-                        break;
-                    case '#':
-                        calculadora = new Calculadora(entradaNumA, entradaNumB);
-                        calculadora.GerarTabuada();
-                        break;
-                    //case '@':
-                    //    calculadora = new Calculadora(descricao);
-                    //    calculadora.GerarRelatorio();
-                    //    break;
-                    default:
-                        calculadora = new Calculadora(entradaOperacao);
-                        calculadora.Erro();
-                        continue;
-                }
-                if (entradaOperacao == '#' || entradaOperacao == '+' || entradaOperacao == '-' || entradaOperacao == '*' || entradaOperacao == '/' || entradaOperacao == '%')
-                {
-                    descricao[qntdOperacao] = calculadora.NumA +" "+ calculadora.Operacao+" "+calculadora.NumB + " = " +calculadora.Resultado;
-                    qntdOperacao++;
-                }
-                Console.WriteLine("=============   RESULTADO   =============");
-                if(entradaOperacao != '#')
-                {
-                    Console.WriteLine($" Resultado: {calculadora.NumA} {calculadora.Operacao} {calculadora.NumB} = {calculadora.Resultado}");
-                }
-                Console.WriteLine("=========================================");
                 Console.WriteLine("Aperte enter para uma nova operacao,");
                 Console.WriteLine("ou Ctrl + C para encerrar o console.");
                 Console.ReadLine();
